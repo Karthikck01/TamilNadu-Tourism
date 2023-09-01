f99799b4a785ae23755d9900b0b8f041e918c021
unknown <karthikumar36729@gmail.com>
2023-09-01 19:06:30 +0530

    WTF

--- src/components/Pages/Packages.jsx
+++ src/components/Pages/Packages.jsx
@@ -1,30 +1,31 @@
 import React, { useState } from "react";
-import { Row, Col, Form,Carousel } from "react-bootstrap";
+import { Row, Col, Form, Carousel } from "react-bootstrap";
 import { DestinationData } from "../../DestinationData";
 
 const Packages = () => {
-    const[cityFrom, setCityFrom] = useState('Chennai');
+  const [cityFrom, setCityFrom] = useState("Chennai");
   return (
     <>
       <h1 className="text-center my-3"> TRIP PACKAGES </h1>
       <Row className="my-3">
-        <Col xs={12} sm={6} >
+        <Col xs={12} sm={6}>
           <div className="py-5">
-            <div className="h4 text-center my-3">
-                Select Your City
-            </div>
-            <Form.Select size="lg" onChange={(e) => setCityFrom(e.target.value)}>
-                {DestinationData.map((city) => {
-                    return(
-                        <option>{city.DestinationDetails.name}</option>
-                    )
-                })}
+            <div className="h4 text-center my-3">Select Your City</div>
+            <Form.Select
+              size="lg"
+              onChange={(e) => setCityFrom(e.target.value)}
+            >
+              {DestinationData.map((city) => {
+                return <option>{city.DestinationDetails.name}</option>;
+              })}
             </Form.Select>
           </div>
         </Col>
-        {DestinationData.filter((city) => city.DestinationDetails.name != cityFrom).map((data, id) => {
+        {DestinationData.filter(
+          (city) => city.DestinationDetails.name != cityFrom
+        ).map((data, id) => {
           return (
-            <Col xs={12} sm={6} key={id} className="package-col">
+            <Col xs={12} sm={6} key={id}>
               <div
                 className="mt-3 p-4 text-light"
                 style={{
@@ -32,7 +33,7 @@ const Packages = () => {
                   minHeight: 360,
                 }}
               >
-                <div className="h3 me-5">
+                <div className="h3 me-5 text-center">
                   {cityFrom} to {data.DestinationDetails.name}
                 </div>
 
@@ -41,33 +42,25 @@ const Packages = () => {
                     <div className="h5">
                       Places in {data.DestinationDetails.name}
                     </div>
-                    {data.PlaceMustVist.map((data,i) => {
-                        return( <div>
-                          {i+1}. {data.title}
-                        </div>)
+                    {data.PlaceMustVist.map((data, i) => {
+                      return (
+                        <div>
+                          {i + 1}. {data.title}
+                        </div>
+                      );
                     })}
                   </div>
 
-                  <div style={{width: 300 , height:200}}> 
-                    <Carousel className="px-5 pb-4">
-                    {data.PlaceMustVist.map((data) => {
-                        return( 
-                          <Carousel.Item>
-                            <img src={data.img} className="w-100" height={140}/>
-                          </Carousel.Item>
-                        )
-                    })}
-                    </Carousel>
-                  </div>
-                </div>
-                <div className="d-flex justify-content-between align-itmes-center ps-4 pe-5">
-                  <div className="h3">
-                    Price:  &#8377;{Math.floor(Math.random() * (9000 - 5000) + 5000).toLocaleString()}
-                  </div>
-                  <div className="">
-                    <div className="btn btn-light w-100">
-                      Book a Trip
+                  <div className="d-flex flex-column ">
+                    <div className="h3 me-5">
+                      Price: <span className="h1">
+                        &#8377;
+                      {Math.floor(
+                        Math.random() * (9000 - 5000) + 5000
+                        ).toLocaleString()}
+                        </span>
                     </div>
+                    <div className="btn btn-light w-100 mt-4">Book a Trip</div>
                   </div>
                 </div>
               </div>
